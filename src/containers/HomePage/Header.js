5dc04c3f8f3ce37af5aad47a24e09682ae3a327a
ThanhLiem <phamvanliem26122002@gmail.com>
2022-12-21 22:47:26 +0700

    init ui banner

--- src/containers/HomePage/Header.js
+++ src/containers/HomePage/Header.js
@@ -4,10 +4,11 @@ import { Col, Row } from 'antd';
 
 const HomePage = () => {
     return (
-        <div className="home-header-container">
+        <>
+          <div className="home-header-container">
             <div className="home_header_content">
                 <div className="header_logo">
-                <i class="fas fa-bars"></i>
+                <i className="fas fa-bars"></i>
                 <div className="header_logo_img"></div>
                 </div>
                 <div className="header_handle">
@@ -29,11 +30,82 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div className="header_support">
-                <i class="fas fa-question"></i>
+                <i className="fas fa-question"></i>
                 <span>Support</span>
                 </div>
             </div>
         </div>
+        <div className="header_background">
+            <div className="header_background-content">
+            <div className="header_title">
+                <h3>NỀN TẢNG Y TẾ</h3>
+                <p>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</p>
+                <div className="header_search">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                    <input placeholder="Tìm bệnh viện"/>
+                </div>
+            </div>
+            <div className="header_handle">
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-h-square" aria-hidden="true"></i>
+                    </div>
+                    <p>Khám <br />Chuyên khoa</p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-phone" aria-hidden="true"></i>
+                    </div>
+                    <p>Khám <br />từ xa </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-address-book" aria-hidden="true"></i>
+                    </div>
+                    <p>Khám <br />tổng quát </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-address-book" aria-hidden="true"></i>
+                    </div>
+                    <p>Xét nghiệm <br />y học </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-user" aria-hidden="true"></i>
+                    </div>
+                    <p> Sức khỏe <br />tinh thần </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-h-square" aria-hidden="true"></i>
+                    </div>
+                    <p>Khám <br />nha khoa </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-h-square" aria-hidden="true"></i>
+                    </div>
+                    <p> Gói <br />Phẫu thuật </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-car" aria-hidden="true"></i>
+                    </div>
+                    <p> Sản phẩm <br />Y tế </p>
+                </div>
+                <div className="form_group">
+                    <div className="icon">
+                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                    </div>
+                    <p>  Sức khỏe <br />Doanh nghiệp</p>
+                </div>
+            </div>
+            </div>
+            
+        </div>
+        </>
+      
     )
 }
 
