4c304418364a5482c45733215cf7fd0bb4a4a17d
ThanhLiem <phamvanliem26122002@gmail.com>
2022-12-22 22:47:50 +0700

    add handle language

--- src/containers/HomePage/Header.js
+++ src/containers/HomePage/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './Header.scss';
-import { Col, Row } from 'antd';
+import { useSelector, useDispatch } from "react-redux";
+import { FormattedMessage } from "react-intl";
 
 const HomePage = () => {
+    const language = useSelector((state) => state.app.language);
+
     return (
         <>
           <div className="home-header-container">
@@ -13,33 +16,37 @@ const HomePage = () => {
                 </div>
                 <div className="header_handle">
                     <div className="child_content">
-                        <h2>Chuyên khoa</h2>
-                        <p>Tìm bác sĩ theo chuyên khoa</p>
+                        <h2><FormattedMessage id="home-header.speciality"/></h2>
+                        <p><FormattedMessage id="home-header.search-doctor"/></p>
                     </div>
                     <div className="child_content">
-                        <h2>Cơ sở y tế </h2>
-                        <p>Chọn bệnh viện phòng khám</p>
+                        <h2><FormattedMessage id="home-header.health-facilities"/></h2>
+                        <p><FormattedMessage id="home-header.hospital-clnic"/></p>
                     </div>
                     <div className="child_content">
-                        <h2>Bác sĩ</h2>
-                        <p>Chọn bác sĩ giỏi</p>
+                        <h2><FormattedMessage id="home-header.doctor"/></h2>
+                        <p><FormattedMessage id="home-header.doctor-good"/></p>
                     </div>
                     <div className="child_content">
-                        <h2>Gói khám</h2>
-                        <p>Khám sức khoẻ tổng quát</p>
+                        <h2><FormattedMessage id="home-header.checkup-package"/></h2>
+                        <p><FormattedMessage id="home-header.health-check"/></p>
                     </div>
                 </div>
                 <div className="header_support">
                 <i className="fas fa-question"></i>
-                <span>Support</span>
+                <span><FormattedMessage id="home-header.support"/></span>
+                <div className="language">
+                    <span>VN</span>
+                    <span>EN</span>
+                </div>
                 </div>
             </div>
         </div>
         <div className="header_background">
             <div className="header_background-content">
             <div className="header_title">
-                <h3>NỀN TẢNG Y TẾ</h3>
-                <p>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</p>
+                <h3><FormattedMessage id="banner.medical-background"/></h3>
+                <p><FormattedMessage id="banner.headlth-care"/></p>
                 <div className="header_search">
                     <i className="fa fa-search" aria-hidden="true"></i>
                     <input placeholder="Tìm bệnh viện"/>
@@ -50,55 +57,37 @@ const HomePage = () => {
                     <div className="icon">
                     <i className="fa fa-h-square" aria-hidden="true"></i>
                     </div>
-                    <p>Khám <br />Chuyên khoa</p>
+                    <p><FormattedMessage id="banner.child-1"/></p>
                 </div>
                 <div className="form_group">
                     <div className="icon">
                     <i className="fa fa-phone" aria-hidden="true"></i>
                     </div>
-                    <p>Khám <br />từ xa </p>
+                    <p><FormattedMessage id="banner.child-2"/></p>
                 </div>
                 <div className="form_group">
                     <div className="icon">
                     <i className="fa fa-address-book" aria-hidden="true"></i>
                     </div>
-                    <p>Khám <br />tổng quát </p>
+                    <p><FormattedMessage id="banner.child-3"/></p>
                 </div>
                 <div className="form_group">
                     <div className="icon">
                     <i className="fa fa-address-book" aria-hidden="true"></i>
                     </div>
-                    <p>Xét nghiệm <br />y học </p>
+                    <p><FormattedMessage id="banner.child-4"/></p>
                 </div>
                 <div className="form_group">
                     <div className="icon">
                     <i className="fa fa-user" aria-hidden="true"></i>
                     </div>
-                    <p> Sức khỏe <br />tinh thần </p>
+                    <p> <FormattedMessage id="banner.child-5"/></p>
                 </div>
                 <div className="form_group">
                     <div className="icon">
                     <i className="fa fa-h-square" aria-hidden="true"></i>
                     </div>
-                    <p>Khám <br />nha khoa </p>
-                </div>
-                <div className="form_group">
-                    <div className="icon">
-                    <i className="fa fa-h-square" aria-hidden="true"></i>
-                    </div>
-                    <p> Gói <br />Phẫu thuật </p>
-                </div>
-                <div className="form_group">
-                    <div className="icon">
-                    <i className="fa fa-car" aria-hidden="true"></i>
-                    </div>
-                    <p> Sản phẩm <br />Y tế </p>
-                </div>
-                <div className="form_group">
-                    <div className="icon">
-                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
-                    </div>
-                    <p>  Sức khỏe <br />Doanh nghiệp</p>
+                    <p><FormattedMessage id="banner.child-6"/></p>
                 </div>
             </div>
             </div>
