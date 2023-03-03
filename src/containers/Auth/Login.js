55a3ee977fa2be7592d6782556bf8b3bc5930684
Thành Liêm <phamvanliem26122002@gmail.com>
2023-03-03 07:09:06 +0000

    Update Login.js

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -52,7 +52,7 @@ class Login extends Component {
       <div className="login-background">
         <div className="login-container">
           <div className="login-content row ">
-            <div className="col-12 text-login">Login</div>
+            <div className="col-12 text-login">Login haha</div>
             <div className="col-12 form-group login-input">
               <label>Username:</label>
               <input
